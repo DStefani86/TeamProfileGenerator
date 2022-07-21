b1cc412410663ae4c52b599c98d1797bfeafe3aa
Mona Lisa <stefanibusiness@gmail.com>
2022-07-20 20:06:39 -0700

    added htmlGen and functions to prompts

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import questions from './src/questions.js';
 import inquirer from 'inquirer';
-// need import classes
-const postions = {Managers:[],Engineers:[],Interns:[]};
+import Manager from './lib/Manager.js'
+import Engineer from './lib/Engineer.js'
+import Intern from './lib/Intern.js'
+import htmlGen from './src/htmlGen.js'
 
-inquirer.prompt(questions).then(ans=>{
-    if(!ans.fillPosition) console.log(ans);
-})
+const positions = {Managers:[],Engineers:[],Interns:[]};
 
+const init = () => {
+    inquirer.prompt(questions).then((ans)=>{
+        const {fillPosition,role,name,id,email} = ans;
+
+        if(!fillPosition) return htmlGen(positions);
+
+        role == 'Manager'
+        ? positions.Managers.push(new Manager(name,id,email,ans.officeNumber))
+        : role == 'Engineer'
+        ? positions.Engineers.push(new Engineer(name,id,email,ans.github))
+        : positions.Interns.push(new Intern(name,id,email,ans.school))
+
+
+        init();
+    })
+};
+
+init ();
---
+++ src/htmlGen.js
@@ -0,0 +1,107 @@
+import { writeFile } from "fs";
+
+const htmlGen = ({ Managers, Engineers, Interns }) => {
+  let output = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <title>Team Profile Generator</title>
+  </head>
+  <body>
+    <div class="jumbotron center jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-8">Team Profile</h1>
+      </div>
+    </div>
+`;
+
+  if (Managers.length) {
+    output += '<div class="row">';
+
+    Managers.forEach((mgr) => {
+      output += ` <div class="card" style="width: 18rem">
+        <div class="card-body">
+        <h2>${mgr.getName()}</h2>
+          <h5 class="emp-title">Manager</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID#: ${mgr.getId()} </li>
+          <li class="list-group-item">
+            <a href="mailto: ${mgr.getEmail()}" class="email-link">E-Mail: ${mgr.getEmail()} </a>
+          </li>
+          <li class="list-group-item">Office Numer: ${mgr.getOfficeNumber()} </li>
+        </ul>
+        <div class="card-body"></div>
+      </div>`;
+    });
+
+    output += "<h1>Managers</h1> <hr> </div>";
+  }
+
+  if (Engineers.length) {
+    output += '<div class="row">';
+
+    Engineers.forEach((eng) => {
+      output += ` <div class="card" style="width: 18rem">
+        <div class="card-body">
+        <h2>${eng.getName()}</h2>
+          <h5 class="emp-title">Engineer</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID#: ${eng.getId()} </li>
+          <li class="list-group-item">
+            Email:<a href="mailto:${eng.getEmail()}" class="email-link"> ${eng.getEmail()} </a>
+          </li>
+          <li class="list-group-item">GitHub: <a href="https://github.com/${eng.getGithub()}"> ${eng.getGithub()}</a></li>
+        </ul>
+        <div class="card-body"></div>
+      </div>`;
+    });
+
+    output += "<h1>Engineers</h1> <hr></div>";
+  }
+
+  if (Interns.length) {
+    output += '<div class="row">';
+
+    Interns.forEach((int) => {
+      output += ` <div class="card" style="width: 18rem">
+        <div class="card-body">
+        <h2>${int.getName()}</h2>
+          <h5 class="emp-title">Intern</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID#: ${int.getId()} </li>
+          <li class="list-group-item">
+            Email:<a href="mailto:${int.getEmail()}" class="email-link"> ${int.getEmail()} </a>
+          </li>
+          <li class="list-group-item">School: ${int.getSchool()}</li>
+        </ul>
+        <div class="card-body"></div>
+      </div>`;
+    });
+
+    output += "<h1>Engineers</h1> <hr> </div>";
+  }
+
+  output += `
+    <script src="./index.js"></script>
+  </body>
+</html>
+`;
+
+  writeFile("./dist/team.html", output, () =>
+    console.log("created html file...")
+  );
+};
+
+export default htmlGen;
